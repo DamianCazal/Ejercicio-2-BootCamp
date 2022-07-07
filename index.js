e0e39fbf5fc15f59d358db7f2f551ddf8f31c300
Damian Cazal <damiancazal21@gmail.com>
2022-07-07 11:51:35 -0300

    commit inicial

---
+++ index.js
@@ -0,0 +1,12 @@
+const lista = [
+  "damian",
+  24,
+  false,
+  new Date(1998,3,21),
+  {
+    titulo:"Análisis Técnico de los Mercados Financieros",
+    autor:"John J. Murphy",
+    fecha: new Date(1986,0,1),
+    url:"https://www.amazon.com/-/es/John-J-Murphy/dp/8498754283",
+  }
+];
